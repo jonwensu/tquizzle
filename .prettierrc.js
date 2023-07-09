47dff1e9dfc5ba148a82b2d45c09c0981a1298d4
noxfur <wens.sumande@gmail.com>
2023-07-09 13:13:15 +0800

    build: create base project

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  quoteProps: 'consistent',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  endOfLine: 'auto',
+  printWidth: 100,
+  plugins: [require('prettier-plugin-tailwindcss')]
+};
---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})({});
+
+module.exports = nextConfig;
